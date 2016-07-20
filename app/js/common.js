f88538dd73ef06db948e62fd0c493f342367c5eb
my name <mymail@mail.ru>
2016-07-20 15:47:04 +0300

    First and Last

---
+++ app/js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$(window).scroll(function(event) {
+		var y = $(this).scrollTop();
+		if (y >= 400) {
+			$('#phone').addClass('animate');			
+			$('#tablet').addClass('animate');
+		}
+	})
+})
